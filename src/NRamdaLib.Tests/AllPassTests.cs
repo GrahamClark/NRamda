cbbf0bedb7a09bb9599eebaf27cf60b2af6f1011
Graham Clark <grahamclarkuk@gmail.com>
2017-06-04 22:03:05 +0100

    uncurried AllPass

---
+++ src/NRamdaLib.Tests/AllPassTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+using FluentAssertions;
+
+using Xunit;
+
+namespace NRamdaLib.Tests
+{
+    public class AllPassTests
+    {
+        private readonly Func<int, bool> _odd = x => x % 2 != 0;
+        private readonly Func<int, bool> _lt20 = x => x < 20;
+        private readonly Func<int, bool> _gt5 = x => x > 5;
+        private readonly Func<int, int, int, int, bool> _plusEq = (w, x, y, z) => w + x == y + z;
+
+        [Fact]
+        public void CanTestTwoPredicates()
+        {
+            // slightly modified from the example in the documentation
+            // as this is using a class instead of an associative array
+            Func<Card, bool> isQueen = c => c.Rank == 'Q';
+            Func<Card, bool> isSpade = c => c.Suit == '♠';
+
+            var isQueenOfSpades = NRamda.AllPass(new[] {isQueen, isSpade});
+
+            isQueenOfSpades(new Card {Rank = 'Q', Suit = '♣'}).Should().BeFalse();
+            isQueenOfSpades(new Card {Rank = 'Q', Suit = '♠' }).Should().BeTrue();
+        }
+
+        [Fact]
+        public void ReportsWhetherAllPredicatesAreSatisfiedByAGivenValue()
+        {
+            var ok = NRamda.AllPass(new[] {_odd, _lt20, _gt5});
+
+            ok(7).Should().BeTrue();
+            ok(9).Should().BeTrue();
+            ok(10).Should().BeFalse();
+            ok(3).Should().BeFalse();
+            ok(21).Should().BeFalse();
+        }
+
+        [Fact]
+        public void ReturnsTrueOnEmptyPredicateList()
+        {
+            NRamda.AllPass(new Func<int,bool>[0])(3).Should().BeTrue();
+        }
+
+        private class Card
+        {
+            public char Rank { get; set; }
+
+            public char Suit { get; set; }
+        }
+    }
+}
---
+++ src/NRamdaLib/NRamda.AllPass.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace NRamdaLib
+{
+    public static partial class NRamda
+    {
+        public static Func<T, bool> AllPass<T>(Func<T, bool>[] predicates)
+        {
+            return x =>
+            {
+                foreach (var predicate in predicates)
+                {
+                    if (!predicate(x))
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            };
+        }
+
+        // Really difficult to take an array of arbitrary-arity predicates.
+        // A custom delegate can be defined, but then everything needs to be
+        // defined in terms of this instead of being able to use implicit 
+        // "casting" to Func<..., bool>. Maybe there's a way to implicitly 
+        // cast a delegate to a Func?
+        public static Func<T1, T2, bool> AllPass<T1, T2>(Func<T1, T2, bool>[] predicates)
+        {
+            return (x, y) =>
+            {
+                foreach (var predicate in predicates)
+                {
+                    if (!predicate(x, y))
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            };
+        }
+
+        public static Func<T1, T2, T3, bool> AllPass<T1, T2, T3>(
+            Func<T1, T2, T3, bool>[] predicates)
+        {
+            return (x, y, z) =>
+            {
+                foreach (var predicate in predicates)
+                {
+                    if (!predicate(x, y, z))
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            };
+        }
+
+        public static Func<T1, T2, T3, T4, bool> AllPass<T1, T2, T3, T4>(
+            Func<T1, T2, T3, T4, bool>[] predicates)
+        {
+            return (w, x, y, z) =>
+            {
+                foreach (var predicate in predicates)
+                {
+                    if (!predicate(w, x, y, z))
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            };
+        }
+    }
+}
