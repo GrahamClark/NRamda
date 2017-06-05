fc1cf5579396e7978c421a9e9ba76ac35ff9fd2a
Graham Clark <grahamclarkuk@gmail.com>
2017-06-05 22:07:51 +0100

    implemented And

---
+++ src/NRamdaLib.Tests/AndTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+
+using Xunit;
+
+namespace NRamdaLib.Tests
+{
+    public class AndTests
+    {
+        [Fact]
+        public void ComparesTwoValuesWithLogicalAnd()
+        {
+            NRamda.And(true, true).Should().BeTrue();
+            NRamda.And(true, false).Should().BeFalse();
+            NRamda.And(false, true).Should().BeFalse();
+            NRamda.And(false, false).Should().BeFalse();
+        }
+
+        [Fact]
+        public void AndIsCurried()
+        {
+            var halfTruth = NRamda.And(true);
+
+            halfTruth(false).Should().BeFalse();
+            halfTruth(true).Should().BeTrue();
+        }
+    }
+}
---
+++ src/NRamdaLib/NRamda.And.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NRamdaLib
+{
+    public static partial class NRamda
+    {
+        public static bool And(bool x, bool y)
+        {
+            return x && y;
+        }
+
+        public static Func<bool, bool> And(bool x)
+        {
+            return Curry<bool, bool, bool>(And)(x);
+        }
+    }
+}
