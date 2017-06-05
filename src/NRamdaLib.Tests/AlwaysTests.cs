cbab39617aa785e6127e75751b685ca82306c985
Graham Clark <grahamclarkuk@gmail.com>
2017-06-05 21:59:40 +0100

    implemented Always

---
+++ src/NRamdaLib.Tests/AlwaysTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+
+using Xunit;
+
+namespace NRamdaLib.Tests
+{
+    public class AlwaysTests
+    {
+        [Fact]
+        public void AlwaysReturnsGivenValue()
+        {
+            var t = NRamda.Always("Tee");
+
+            t().Should().Be("Tee");
+            t().Should().Be("Tee");
+        }
+
+        [Fact]
+        public void AlwaysWithOneParameter()
+        {
+            var t = NRamda.Always<int, string>("Tee");
+
+            t(1).Should().Be("Tee");
+            t(456).Should().Be("Tee");
+        }
+
+        [Fact]
+        public void AlwaysWithTwoParametersIsCurried()
+        {
+            var t = NRamda.Always<int, bool, string>("Tee");
+
+            t(3)(true).Should().Be("Tee");
+            t(3939)(false).Should().Be("Tee");
+        }
+    }
+}
---
+++ src/NRamdaLib/NRamda.Always.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NRamdaLib
+{
+    public static partial class NRamda
+    {
+        public static Func<T1> Always<T1>(T1 value)
+        {
+            return () => value;
+        }
+
+        public static Func<T1, T2> Always<T1, T2>(T2 value)
+        {
+            return x => value;
+        }
+
+        public static Func<T1, Func<T2, T3>> Always<T1, T2, T3>(T3 value)
+        {
+            return Curry<T1, T2, T3>((x, y) => value);
+        }
+    }
+}
