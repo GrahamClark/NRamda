50c52f7098e0241592471db3c2788d588387a34f
Graham Clark <grahamclarkuk@gmail.com>
2017-08-15 21:30:18 +0100

    implemented Aperture

---
+++ src/NRamdaLib.Tests/ApertureTests.cs
@@ -0,0 +1,62 @@
+﻿using FluentAssertions;
+
+using Xunit;
+
+namespace NRamdaLib.Tests
+{
+    public class ApertureTests
+    {
+        private readonly int[] _list = { 1, 2, 3, 4, 5, 6, 7 };
+
+        [Fact]
+        public void ReturnsEmptyCollectionWhenNIsLargerThanCollectionLength()
+        {
+            NRamda.Aperture(4, new[] { 1, 2, 3 }).Should().BeEmpty();
+            NRamda.Aperture(1, new int[0]).Should().BeEmpty();
+        }
+
+        [Fact]
+        public void CreatesACollectionOfNTuplesFromACollection()
+        {
+            NRamda.Aperture(1, _list)
+                  .ShouldBeEquivalentTo(
+                      new[]
+                      {
+                          new[] { 1 }, new[] { 2 }, new[] { 3 }, new[] { 4 }, new[] { 5 },
+                          new[] { 6 }, new[] { 7 }
+                      });
+
+            NRamda.Aperture(2, _list)
+                  .ShouldBeEquivalentTo(
+                      new[]
+                      {
+                          new[] { 1, 2 }, new[] { 2, 3 }, new[] { 3, 4 }, new[] { 4, 5 },
+                          new[] { 5, 6 }, new[] { 6, 7 }
+                      });
+
+            NRamda.Aperture(3, _list)
+                  .ShouldBeEquivalentTo(
+                      new[]
+                      {
+                          new[] { 1, 2, 3 }, new[] { 2, 3, 4 }, new[] { 3, 4, 5 },
+                          new[] { 4, 5, 6 }, new[] { 5, 6, 7 }
+                      });
+
+            NRamda.Aperture(4, new[] { 1, 2, 3, 4 })
+                  .ShouldBeEquivalentTo(new[] { new[] { 1, 2, 3, 4 } });
+        }
+
+        [Fact]
+        public void IsCurried()
+        {
+            var pairwise = NRamda.Aperture<int>(2);
+            pairwise(_list)
+                .ShouldBeEquivalentTo(
+                    new[]
+                    {
+                        new[] { 1, 2 }, new[] { 2, 3 }, new[] { 3, 4 }, new[] { 4, 5 },
+                        new[] { 5, 6 }, new[] { 6, 7 }
+                    });
+        }
+    }
+}
---
+++ src/NRamdaLib/NRamda.Aperture.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NRamdaLib
+{
+    public static partial class NRamda
+    {
+        public static IEnumerable<IEnumerable<T>> Aperture<T>(this int n, IEnumerable<T> collection)
+        {
+            var list = collection.ToList();
+            if (n > list.Count)
+            {
+                return Enumerable.Empty<IEnumerable<T>>();
+            }
+
+            return ApertureInternal(n, list);
+        }
+
+        public static Func<IEnumerable<T>, IEnumerable<IEnumerable<T>>> Aperture<T>(this int n)
+        {
+            return Curry<int, IEnumerable<T>, IEnumerable<IEnumerable<T>>>(Aperture)(n);
+        }
+
+        private static IEnumerable<IEnumerable<T>> ApertureInternal<T>(int n, List<T> list)
+        {
+            int index = 0;
+            int limit = list.Count - (n - 1);
+            while (index < limit)
+            {
+                yield return list.Skip(index).Take(n);
+                index += 1;
+            }
+        }
+    }
+}
