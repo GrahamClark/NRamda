dc2f33cfffdc5ae6228f8b5c608c683be3c89441
Graham Clark <grahamclarkuk@gmail.com>
2017-08-23 21:47:03 +0100

    implemented Append

---
+++ src/NRamdaLib.Tests/AppendTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+
+using Xunit;
+
+namespace NRamdaLib.Tests
+{
+    public class AppendTests
+    {
+        [Fact]
+        public void CanAppendItemToList()
+        {
+            var appendedInt = 4.Append(new[] { 1, 2, 3 });
+            appendedInt.ShouldBeEquivalentTo(new[] { 1, 2, 3, 4 });
+
+            var appendedString = "you".Append(new[] { "hello", "there" });
+            appendedString.ShouldAllBeEquivalentTo(new[] { "hello", "there", "you" });
+        }
+
+        [Fact]
+        public void WorksOnAnEmptyList()
+        {
+            1.Append(new int[0]).ShouldBeEquivalentTo(new[] { 1 });
+        }
+
+        [Fact]
+        public void IsCurried()
+        {
+            var appender = 4.Append();
+            appender(new[] { 1, 2, 3 }).ShouldBeEquivalentTo(new[] { 1, 2, 3, 4 });
+        }
+    }
+}
---
+++ src/NRamdaLib/NRamda.Append.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NRamdaLib
+{
+    public static partial class NRamda
+    {
+        public static IEnumerable<T> Append<T>(this T item, IEnumerable<T> list)
+        {
+            foreach (var listItem in list)
+            {
+                yield return listItem;
+            }
+
+            yield return item;
+        }
+
+        public static Func<IEnumerable<T>, IEnumerable<T>> Append<T>(this T item)
+        {
+            return Curry<T, IEnumerable<T>, IEnumerable<T>>(Append)(item);
+        }
+    }
+}
