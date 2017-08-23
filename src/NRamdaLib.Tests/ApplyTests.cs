81ee48dfcaf785fc916e122a3481ebd039ea0afa
Graham Clark <grahamclarkuk@gmail.com>
2017-08-23 22:19:18 +0100

    implemented Apply

---
+++ src/NRamdaLib.Tests/ApplyTests.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+
+using Xunit;
+
+namespace NRamdaLib.Tests
+{
+    public class ApplyTests
+    {
+        [Fact]
+        public void AppliesFunctionToArgumentList()
+        {
+            NRamda.Apply(VariadicMax, new[] { 1, 2, 3, -99, 42, 6, 7 }).Should().Be(42);
+        }
+
+        [Fact]
+        public void IsCurried()
+        {
+            NRamda.Apply<int, int>(VariadicMax)(new[] { 1, 2, 3, -99, 42, 6, 7 }).Should().Be(42);
+        }
+
+        private static int VariadicMax(params int[] numbers)
+        {
+            if (numbers.Length == 1)
+            {
+                return numbers[0];
+            }
+
+            int max = numbers[0];
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] > max)
+                {
+                    max = numbers[i];
+                }
+            }
+
+            return max;
+        }
+    }
+}
---
+++ src/NRamdaLib/NRamda.Apply.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NRamdaLib
+{
+    public static partial class NRamda
+    {
+        public static TResult Apply<TList, TResult>(
+            this Func<IEnumerable<TList>, TResult> variadic,
+            IEnumerable<TList> list)
+        {
+            return variadic(list);
+        }
+
+        public static TResult Apply<TList, TResult>(
+            this Func<TList[], TResult> variadic,
+            TList[] list)
+        {
+            return variadic(list);
+        }
+
+        public static Func<IEnumerable<TList>, TResult> Apply<TList, TResult>(
+            this Func<IEnumerable<TList>, TResult> variadic)
+        {
+            return Curry<Func<IEnumerable<TList>, TResult>, IEnumerable<TList>, TResult>(Apply)(
+                variadic);
+        }
+
+        public static Func<TList[], TResult> Apply<TList, TResult>(
+            this Func<TList[], TResult> variadic)
+        {
+            return Curry<Func<TList[], TResult>, TList[], TResult>(Apply)(variadic);
+        }
+    }
+}
