842d3eca9e2db37a59cb64c35610d4b21f746bff
Graham Clark <grahamclarkuk@gmail.com>
2017-08-24 21:40:38 +0100

    implemented ApplySpec

---
+++ src/NRamdaLib.Tests/ApplySpecTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+using FluentAssertions;
+
+using Xunit;
+
+namespace NRamdaLib.Tests
+{
+    public class ApplySpecTests
+    {
+        [Fact]
+        public void CanApplySpecificationToList()
+        {
+            Func<int, int, int> f1 = NRamda.Add;
+            Func<int, int, int> f2 = NRamda.Multiply;
+            var spec = new Dictionary<string, object>
+            {
+                ["sum"] = f1,
+                ["nested"] = new Dictionary<string, object>
+                {
+                    ["mul"] = f2
+                }
+            };
+
+            var result = spec.ApplySpec<int, int>(new[] { 2, 4 });
+            result.ShouldBeEquivalentTo(new Dictionary<string, object>
+            {
+                ["sum"] = 6,
+                ["nested"] = new Dictionary<string, object>
+                {
+                    ["mul"] = 8
+                }
+            });
+        }
+    }
+}
---
+++ src/NRamdaLib/NRamda.ApplySpec.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NRamdaLib
+{
+    public static partial class NRamda
+    {
+        // This is almost completely pointless in C#. Oh well! Got to try some pattern matching.
+        public static IDictionary<string, object> ApplySpec<T1, T2>(
+            this IDictionary<string, object> spec,
+            IEnumerable<T1> collection)
+        {
+            var list = collection.ToList();
+            if (list.Count != 2)
+            {
+                throw new InvalidOperationException($"{nameof(collection)} count must be 2");
+            }
+
+            return ApplySpec<T1, T2>(spec, list, new Dictionary<string, object>());
+        }
+
+        public static Func<IEnumerable<T1>, IDictionary<string, object>> ApplySpec<T1, T2>(
+            this IDictionary<string, object> spec)
+        {
+            return Curry<IDictionary<string, object>, IEnumerable<T1>, IDictionary<string, object>>(
+                ApplySpec<T1, T2>)(spec);
+        }
+
+        private static IDictionary<string, object> ApplySpec<T1, T2>(
+            IDictionary<string, object> spec,
+            List<T1> list,
+            IDictionary<string, object> result)
+        {
+            foreach (var kvp in spec)
+            {
+                switch (kvp.Value)
+                {
+                    case Func<T1, T1, T2> func:
+                        result[kvp.Key] = func(list[0], list[1]);
+                        break;
+
+                    case IDictionary<string, object> dict:
+                        result[kvp.Key] = ApplySpec<T1, T2>(
+                            dict,
+                            list,
+                            new Dictionary<string, object>());
+                        break;
+
+                    default:
+                        result[kvp.Key] = kvp.Value;
+                        break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
