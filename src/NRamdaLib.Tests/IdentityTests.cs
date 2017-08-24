c55d2ef5a9d97e5f221b1728d93074ad5c11bf0d
Graham Clark <grahamclarkuk@gmail.com>
2017-08-24 21:49:05 +0100

    implemented Identity

---
+++ src/NRamdaLib.Tests/IdentityTests.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+
+using Xunit;
+
+namespace NRamdaLib.Tests
+{
+    public class IdentityTests
+    {
+        [Fact]
+        public void ReturnsParameter()
+        {
+            NRamda.Identity(1).Should().Be(1);
+            NRamda.Identity("yo").Should().Be("yo");
+            NRamda.Identity("hello", "there").Should().Be("hello");
+        }
+    }
+}
---
+++ src/NRamdaLib/NRamda.Identity.cs
@@ -0,0 +1,15 @@
+﻿namespace NRamdaLib
+{
+    public static partial class NRamda
+    {
+        public static T Identity<T>(T t)
+        {
+            return t;
+        }
+
+        public static T Identity<T>(params T[] ts)
+        {
+            return ts[0];
+        }
+    }
+}
