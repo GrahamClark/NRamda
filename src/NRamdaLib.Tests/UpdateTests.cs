53d9bb28e6416477e958c326644eca14fd53f0bd
Graham Clark <grahamclarkuk@gmail.com>
2017-08-28 21:34:30 +0100

    implemented Update

---
+++ src/NRamdaLib.Tests/UpdateTests.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+
+using Xunit;
+
+namespace NRamdaLib.Tests
+{
+    public class UpdateTests
+    {
+        [Fact]
+        public void CanUpdateListValue()
+        {
+            NRamda.Update(1, 11, new[] { 0, 1, 2 }).ShouldBeEquivalentTo(new[] { 0, 11, 2 });
+        }
+
+        [Fact]
+        public void IsCurried()
+        {
+            NRamda.Update<int>(1)(11)(new[] { 0, 1, 2 }).ShouldBeEquivalentTo(new[] { 0, 11, 2 });
+        }
+
+        [Fact]
+        public void OffsetsNegativeIndexesFromTheEndOfTheList()
+        {
+            NRamda.Update(-3, 4, new[] { 0, 1, 2, 3 }).ShouldBeEquivalentTo(new[] { 0, 4, 2, 3 });
+        }
+
+        [Fact]
+        public void DoesNotMutateTheOriginalCollection()
+        {
+            var list = new[] { 0, 1, 2, 3 };
+            NRamda.Update(2, 4, list).ShouldBeEquivalentTo(new[] { 0, 1, 4, 3 });
+            list.ShouldBeEquivalentTo(new[] { 0, 1, 2, 3 });
+        }
+
+        [Fact]
+        public void ReturnsTheOriginalListIfTheIndexIsOutOfBounds()
+        {
+            var list = new[] { 0, 1, 2, 3 };
+            NRamda.Update(4, 4, list).ShouldBeEquivalentTo(list);
+            NRamda.Update(-5, 4, list).ShouldBeEquivalentTo(list);
+        }
+    }
+}
---
+++ src/NRamdaLib/NRamda.Update.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NRamdaLib
+{
+    public static partial class NRamda
+    {
+        /// <summary>
+        /// Returns a new copy of the collection with the element at the provided index 
+        /// replaced with the given value.
+        /// </summary>
+        /// <typeparam name="T">The type of the collection items.</typeparam>
+        /// <param name="index">The index to update.</param>
+        /// <param name="value">The new value.</param>
+        /// <param name="collection">The collection.</param>
+        public static IEnumerable<T> Update<T>(int index, T value, IEnumerable<T> collection)
+        {
+            var newCollection = collection.ToList();
+            if (index < 0)
+            {
+                index = newCollection.Count + index;
+            }
+
+            if (index >= newCollection.Count || index < 0)
+            {
+                return newCollection;
+            }
+
+            newCollection[index] = value;
+
+            return newCollection;
+        }
+
+        /// <summary>
+        /// Takes an index and returns a curried version of Update.
+        /// </summary>
+        /// <typeparam name="T">The type of the collection items.</typeparam>
+        /// <param name="index">The index to update.</param>
+        public static Func<T, Func<IEnumerable<T>, IEnumerable<T>>> Update<T>(int index)
+        {
+            return Curry<int, T, IEnumerable<T>, IEnumerable<T>>(Update)(index);
+        }
+    }
+}
