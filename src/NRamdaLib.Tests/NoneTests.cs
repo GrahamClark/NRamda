b09fea6632214eeb41ce968d52127fb69ea5e11e
Graham Clark <grahamclarkuk@gmail.com>
2017-08-28 21:50:08 +0100

    implemented None

---
+++ src/NRamdaLib.Tests/NoneTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+using FluentAssertions;
+
+using Xunit;
+
+namespace NRamdaLib.Tests
+{
+    public class NoneTests
+    {
+        private readonly Func<int, bool> _isEven = x => x % 2 == 0;
+
+        [Fact]
+        public void ReturnsTrueIfNoElementsSatisfyThePredicate()
+        {
+            _isEven.None(new[] { 1, 3, 5, 7, 9, 11 }).Should().BeTrue();
+        }
+
+        [Fact]
+        public void ReturnsFalseIfAnyElementsSatisfyThePredicate()
+        {
+            _isEven.None(new[] { 1, 3, 5, 7, 8, 11 }).Should().BeFalse();
+        }
+
+        [Fact]
+        public void ReturnsTrueForAnEmptyList()
+        {
+            Func<int, bool> t = x => true;
+            t.None(new int[0]).Should().BeTrue();
+        }
+
+        [Fact]
+        public void IsCurried()
+        {
+            NRamda.None(_isEven)(new[] { 1, 3, 5, 6, 7, 9 }).Should().BeFalse();
+        }
+
+        [Fact]
+        public void WorksWithComplexObjects()
+        {
+            var people = new[] { new Person("Bob"), new Person("Henry"), new Person("Alexander") };
+            Func<Person, bool> len4 = p => p.Name.Length == 4;
+            Func<Person, bool> hasE = p => p.Name.Contains("e");
+
+            NRamda.None(len4, people).Should().BeTrue();
+            NRamda.None(hasE, people).Should().BeFalse();
+        }
+
+        private class Person
+        {
+            public Person(string name)
+            {
+                Name = name;
+            }
+
+            public string Name { get; }
+        }
+    }
+}
---
+++ src/NRamdaLib/NRamda.None.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NRamdaLib
+{
+    public static partial class NRamda
+    {
+        /// <summary>
+        /// Tests if no item in a collection satisfies a predicate.
+        /// </summary>
+        /// <typeparam name="T">The type of the collection items.</typeparam>
+        /// <param name="predicate">The predicate to apply to the collection items.</param>
+        /// <param name="collection">The collection to test.</param>
+        public static bool None<T>(this Func<T, bool> predicate, IEnumerable<T> collection)
+        {
+            foreach (var item in collection)
+            {
+                if (predicate(item))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Takes a predicate and returns a function which takes a collection to apply the predicate to
+        /// and tests if no item in the collection satisfies the predicate.
+        /// </summary>
+        /// <typeparam name="T">The type of the collection items.</typeparam>
+        /// <param name="predicate">The predicate.</param>
+        public static Func<IEnumerable<T>, bool> None<T>(this Func<T, bool> predicate)
+        {
+            return Curry<Func<T, bool>, IEnumerable<T>, bool>(All)(predicate);
+        }
+    }
+}
